07ebc4f9d2ed336b6d5a1309a7fef1fb923198de
emewjin <yujinwork03@gmail.com>
2021-07-28 11:18:40 +0900

    Add :파라미터 확인용  정규표현식을 작성한다

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -6,6 +6,9 @@
 import Dashboard from "./views/Dashboard.js";
 import Courses from "./views/Courses.js";
 
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
 const navigateTo = (url) => {
   history.pushState(null, null, url);
   router();
@@ -22,22 +25,24 @@ const router = async () => {
   const potentialMatches = routes.map((route) => {
     return {
       route: route,
-      isMatch: location.pathname === route.path,
+      result: location.pathname.match(pathToRegex(route.path)),
     };
   });
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
 
   // route에 정의된 곳으로 이동하지 않는다면 기본값으로 되돌린다.
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: true,
     };
   }
 
   //view가 class이기 때문에 new 생성자를 사용해서 새 인스턴스를 만든다
-  const view = new match.route.view();
+  const view = new match.route.view(getParams(match));
 
   // async await으로 관리해주지 않으면 자바스크립트가 요소를 찾지 못해 에러가 날까?
   document.querySelector("#app").innerHTML = await view.getHtml();
